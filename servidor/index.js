e0711c815ee8c32037d4e39ce8c9debbb879da74
BallinasvallejoRocio <157338652+BallinasvallejoRocio@users.noreply.github.com>
2024-02-10 22:51:24 -0600

    seguridad morgan y websocket

--- servidor/index.js
+++ servidor/index.js
@@ -1,21 +1,46 @@
 const express = require('express');
+const morgan = require('morgan');
+const { Server: SocketServer } = require('socket.io');
+const http = require('http');
 const cors = require('cors');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const path = require('path');
 
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "*",
+  }
+});
+
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 let notificaciones = [];
 let idNotificacion = 0;
 
+// Socket.IO
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+  socket.setMaxListeners(50);
+  
+  socket.emit('conexionExitosa', 'Nuevo Cliente Conectado');
+
+  socket.on('message', (msg) => {
+    socket.broadcast.emit('message', { body: msg.body, user: msg.user });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../cliente', 'index.html'));
 });
 
-// short-polling
+//short-polling
 app.get('/notificaciones', (req, res) => {
     res.status(200).json({
         success: true,
@@ -23,7 +48,7 @@ app.get('/notificaciones', (req, res) => {
     });
 });
 
-// long-polling
+//long-polling
 app.get('/notificaciones/update', (req, res) => {
     const idNotificacion = parseInt(req.query.idNotificacion, 10);
     const notificacionesNuevas = notificaciones.filter(notificacion => notificacion.id > idNotificacion);
@@ -57,13 +82,8 @@ app.post('/notificaciones', (req, res) => {
     });
 });
 
-http.listen(3000, () => console.log('Servidor en ejecución en el puerto 3000'));
-
-//Socket.IO
-io.on('connection', (socket) => {
-    console.log('Un cliente se ha conectado');
-
-    socket.on('disconnect', () => {
-        console.log('Un cliente se ha desconectado');
-    });
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
